3d5434c9531051d74a8fb2b9827014185f54c489
Vlad <vladyabl15@gmail.com>
2022-01-06 19:08:29 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASPProject.Models.Home;
+
+namespace ASPProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ShopDB db = new ShopDB();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var items = from e in db.Items
+                          orderby e.Id
+                          select e;
+            return View(items);
+        }
+
+        [HttpGet]
+        public ActionResult Items()
+        {
+            var items = from e in db.Items
+                        orderby e.Id
+                        select e;
+            return View(items);
+        }
+
+        [HttpGet]
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return Content("Create");
+            //return View();
+        }
+
+    }
+}
